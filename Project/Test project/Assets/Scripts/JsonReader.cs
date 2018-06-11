c4c91ed05b8452d15f280dfde821a704a0c20022
ColinvD <22950@ma-web.nl>
2018-06-11 12:11:44 +0200

    recieving information from online json file

--- Project/Test project/Assets/Scripts/JsonReader.cs
+++ Project/Test project/Assets/Scripts/JsonReader.cs
@@ -5,29 +5,21 @@ using System.IO;
 
 public class JsonReader : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-        /**
-		 * 1. Fetch JSON-formatted string from text file
-		 */
+    public class values
+    {
+        public int x;
+        public int y;
+        public int width;
+        public int height;
+    }
 
-        // string jsonString = "{ \"name\": \"Fabi\", \"level\": \"4711\", \"tags\": [\"Beginner\",\"Fast\"] }";
-        //string jsonString = File.ReadAllText("http://22950.hosts.ma-cloud.nl/bewijzenmap/test/drawing.json");
-        //Debug.Log(Application.dataPath);
+	// Use this for initialization
+	IEnumerator Start () {
         string url = "http://22950.hosts.ma-cloud.nl/bewijzenmap/test/drawing.json";
         WWW www = new WWW(url);
-
-        /**
-		 * 2. Transform JSON-formatted text into object
-		 */
-
-        /*ObjectData myObject = JsonUtility.FromJson<ObjectData>(jsonString);
-
-        Debug.Log("Whole JSON String: " + jsonString);
-        foreach (string tag in myObject.tags)
-        {
-            Debug.Log("Print List Item: " + tag); // logs Beginner, then Fast
-        }*/
+        yield return www;
+        values obj = JsonUtility.FromJson<values>(www.text);
+        Debug.Log(obj.height);
     }
 	
 	// Update is called once per frame
